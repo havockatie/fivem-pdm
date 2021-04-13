eb9ced1c7674774d3c2a7e1ba2bdf1c1c9ba2f5f
havockatie <havockatie@gmail.com>
2021-04-13 18:33:04 -0400

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,7 @@
+--Strike-Modifications--
+--Support:https://discord.gg/svfef6JtTK
+--Premium-Deluxe-Motorsport-Exterior--
+--Creator On Discord:Cobra |Strike-Modifications|#1234--
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+this_is_a_map 'yes'
